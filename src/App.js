3bb85e2ad9c34943982824fcd01c5b45334e1f23
Abhi727464 <abhaysinghparmar123@gmail.com>
2023-04-16 23:05:14 +0530

    some changement

--- src/App.js
+++ src/App.js
@@ -10,35 +10,32 @@ import UserData from "./UserData/UserData";
 import { ToastContainer, toast } from "react-toastify";
 
 function App() {
-
-  const getData = ()=>{
-    let data =   localStorage.getItem("users") || []
+  const getData = () => {
+    let data = localStorage.getItem("users") || [];
     //  if(data){
-    //   return JSON.parse(data || []) 
+    //   return JSON.parse(data || [])
     //  }
-    console.log(data)
-    return data && data.length ? JSON.parse(data) : []
-     
-    }
+    console.log(data);
+    return data && data.length ? JSON.parse(data) : [];
+  };
 
-  const [users,setUsers] = useState(getData())
+  const [users, setUsers] = useState(getData());
 
-  const [name,setName] = useState("")
-  const [email,setEmail] = useState("")
-  const [mobile,setMobile] = useState("")
-  const [city,setCity] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [city, setCity] = useState("");
 
-
-  const handleSubmit = (e)=>{
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log("btn clicked");
-    let user ={name,email,mobile,city}
-    setUsers([...users,user]) 
+    let user = { name, email, mobile, city };
+    setUsers([...users, user]);
     setName("");
     setEmail("");
-    setMobile("")
-    setCity("")
-    toast.success('Data Send Successfully', {
+    setMobile("");
+    setCity("");
+    toast.success("Data Send Successfully", {
       position: "top-center",
       autoClose: 5000,
       hideProgressBar: false,
@@ -47,22 +44,35 @@ function App() {
       draggable: true,
       progress: undefined,
       theme: "light",
-      });
-  }
-  useEffect(()=>{
-    localStorage.setItem("users",JSON.stringify(users || [])) 
-  },[users]);
-
+    });
+  };
+  useEffect(() => {
+    localStorage.setItem("users", JSON.stringify(users || []));
+  }, [users]);
 
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Home />}></Route>
-          <Route  path="/admin" element={<Admin  users={users}/>}></Route>
-          <Route  path="/about" element={<About />}></Route>
-          <Route  path="/contact" element={<Contact name={name} email={email} mobile={mobile} city={city} handleSubmit={handleSubmit} setName={setName} setEmail={setEmail} setMobile={setMobile} setCity={setCity}/>}></Route>
-          <Route  path="/users" element={<UserData  users={users}/>}></Route>
-        
+        <Route path="/admin" element={<Admin users={users} />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route
+          path="/contact"
+          element={
+            <Contact
+              name={name}
+              email={email}
+              mobile={mobile}
+              city={city}
+              handleSubmit={handleSubmit}
+              setName={setName}
+              setEmail={setEmail}
+              setMobile={setMobile}
+              setCity={setCity}
+            />
+          }
+        ></Route>
+        <Route path="/users" element={<UserData users={users} />}></Route>
       </Routes>
       <ToastContainer />
     </BrowserRouter>
